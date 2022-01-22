c843057709709178937cf73fb10ddb65f2854391
Yaniv <lerman01@gmail.com>
2022-01-22 19:36:37 +0200

    Prevent reject unauthorized requests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import countryCodes from 'country-codes-list'
 import async from "async";
 import fetch from 'node-fetch';
+import https from 'https';
+
 import {
     currentImages,
     downloadImage,
@@ -15,9 +17,13 @@ const PID_LIST = ["338387", "209567", "279978", "209562"];
 const COUNTRY_LIST = countryCodes.all();
 const CONCURRENCY_JOBS = 100;
 
+
+const httpsAgent = new https.Agent({rejectUnauthorized: false});
+
 const getImageUrl = async () => {
     const response = await fetch(`https://arc.msn.com/v3/Delivery/Placement?pid=${PID_LIST[Math.floor(Math.random() * PID_LIST.length)]}&ctry=${COUNTRY_LIST[Math.floor(Math.random() * COUNTRY_LIST.length)].countryCode}&lc=${COUNTRY_LIST[Math.floor(Math.random() * COUNTRY_LIST.length)].officialLanguageCode}&lo=${Math.floor(Math.random() * 10000000)}&fmt=json&cdm=1`,
         {
+            agent: httpsAgent,
             headers: {
                 "accept-language": "en-US,en;q=0.9,he-IL;q=0.8,he;q=0.7",
                 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
